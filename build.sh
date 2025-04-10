858e0069033bc56ec1bf9c92c087e58054d82037
buildwithadi <adityarawat.online@gmail.com>
2025-04-10 16:13:44 +0530

    build.sh added - 2

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Migrate database (if needed)
+python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
