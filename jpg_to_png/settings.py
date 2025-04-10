f7fc878c03139223b1c93f9f990e854498ec5e21
buildwithadi <adityarawat.online@gmail.com>
2025-04-10 17:06:53 +0530

    build.sh added - 11

--- jpg_to_png/settings.py
+++ jpg_to_png/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -127,4 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
