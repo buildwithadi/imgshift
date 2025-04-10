c2bed61a018a7e118437039abb85cd70c4b1a687
buildwithadi <adityarawat.online@gmail.com>
2025-04-10 16:29:37 +0530

    build.sh added - 5

--- jpg_to_png/settings.py
+++ jpg_to_png/settings.py
@@ -124,5 +124,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "converter/static"),
 ]
